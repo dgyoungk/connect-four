a988842fa04f89f493a8fb4187ad7ae0257f192b
Daniel <150309766+dgyoungk@users.noreply.github.com>
2024-02-11 19:53:17 -0800

    feat: add methods for in game message and logic

--- lib/displayable.rb
+++ lib/displayable.rb
@@ -6,21 +6,34 @@ module Displayable
 
   def rules_msg
     puts %(\nIn case you don't know the rules:)
-    puts %(The goal is to get 4 of your tokens in a row)
-    puts %(Horizontally, vertically, or diagonally)
+    puts %(The goal is to get 4 of your tokens in a row, either horizontally, vertically, or diagonally)
     puts %(The first player to get 4 in a row wins!)
+    puts %(You can only place tokens at the bottom of the board or on top of a placed token)
+  end
+
+  def grid_position_msg
+    puts %(Each spot in the board can be indicated with numbers, from 1 to 42)
+    puts %(1 is the top-left corner, and 42 is the bottom-right corner)
   end
 
   def new_player_msg
     print %(Player username: )
   end
 
-  def display_grid
-
+  def display_grid(grid)
+    42.times do |n|
+      if (n + 1) % 7 == 0
+        print %( #{grid[n + 1]} |\n)
+      elsif (n + 1) % 7 == 1
+        print "|" + %( #{grid[n + 1]} |)
+      else
+        print %( #{grid[n + 1]} |)
+      end
+    end
   end
 
-  def marker_msg
-    print %(Choose a color for your token (black/white): )
+  def turn_msg(player)
+    puts %(It's #{player.name}'s turn)
   end
 
   def grid_position_msg
@@ -30,4 +43,5 @@ module Displayable
   def error_msg
     puts %(Invalid option, try again)
   end
+  # TODO: implement rest of the in-game messages
 end
--- lib/thinkable.rb
+++ lib/thinkable.rb
@@ -5,24 +5,38 @@ require_relative 'displayable'
 module Thinkable
   include Displayable
 
+  def empty_token
+    return "\u25EF"
+  end
+
   def get_marker_choices
-    return { "black" => "\u25EF", 'white' => "\u25C9" }
+    return { "red" => "\u24C7", 'yellow' => "\u24CE" }
   end
 
   def verify_choice(input)
     return input == 'black' || input == 'white'
   end
 
-  # this will return the indicated choice back to the Game class
-  # then Game class will call designate_marker on the player object
-  def choose_marker
-    marker_msg
-    choice = gets.chomp.downcase
-    until verify_choice(choice)
-      error_msg
-      marker_msg
-      choice = gets.chomp.downcase
+  def grid_filled?(grid)
+    return grid.all? { |k, v| v != empty_token }
+  end
+
+  # TODO: determine where I want to put the get_grid_position method
+  def position_verified?(position)
+    unless position.between?(1, 7)
+      return grid[position - 7] != player1.mark && grid[position- - 7] != player2.mark
+    else
+      true
     end
-    choice
+  end
+
+  def get_grid_position
+    grid_position_msg
+    player_choice = gets.chomp.to_i
+    until position_verified?(player_choice)
+      error_msg
+      grid_position_msg
+    end
+    player_choice
   end
 end
