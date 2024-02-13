5851ffa73fa3a90bf19af4b71e7a289060bfcacb
Daniel <150309766+dgyoungk@users.noreply.github.com>
2024-02-12 16:39:17 -0800

    chore: test new methods for Board and Player

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,7 +2,7 @@ require './lib/board.rb'
 
 describe Board do
   subject(:new_board) { described_class.new }
-  let(:player) { double('player', name: "dummy", mark: "\u25EF") }
+  let(:player) { double('player', name: "dummy", mark: "\u24C7") }
   let(:desired_board_pos) { 5 }
   describe '#initialize' do
     context 'when a new instance is created' do
@@ -13,8 +13,17 @@ describe Board do
   end
   describe '#update_board' do
     context 'when a player marks a place on a grid' do
-      it 'updates the board hash' do
+      it 'updates the board grid' do
         expect { new_board.update_grid(player, desired_board_pos) }.to change { new_board.grid[desired_board_pos] }.to (player.mark)
+      end
+    end
+  end
+  describe '#reset_grid' do
+    context 'when method executes' do
+      let(:comparing_board) { described_class.new }
+      it 'sets the grid to its initial state' do
+        new_board.reset_grid
+        expect(comparing_board.grid).to eq (new_board.grid)
       end
     end
   end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -5,5 +5,12 @@ require './lib/player.rb'
 # TODO: test method #update_token_locations
 describe Player do
   subject(:new_player) { described_class.new("Dummy") }
-  let(:marker_choices) { double("marker_choices", red: "\u24C7", yellow: "\u24CE") }
+  let(:token_position) { 5 }
+  describe '#update_token_locations' do
+    context 'when method executes' do
+      it 'stores the token position in the placed array' do
+        expect { new_player.update_token_locations(token_position) }.to change { new_player.placed }.to include(token_position)
+      end
+    end
+  end
 end
