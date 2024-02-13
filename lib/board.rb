f510800e4362632808cfa56a588fe2412260c119
Daniel <150309766+dgyoungk@users.noreply.github.com>
2024-02-12 16:40:29 -0800

    chore: add gameplay logic to Game class

--- lib/board.rb
+++ lib/board.rb
@@ -10,6 +10,10 @@ class Board
     game_grid = 42.times.with_object(Hash.new) { |n, hash| hash[n + 1] = "\u25EF" }
   end
 
+  def reset_grid
+    self.grid.each { |k, v| v = "\u25EF" }
+  end
+
   def update_grid(player, position)
     grid[position] = player.mark
   end
--- lib/game.rb
+++ lib/game.rb
@@ -7,12 +7,12 @@ class Game
   include Displayable
   include Thinkable
 
-  attr_accessor :player1, :player2, :grid, :replay, :match_finished, :turns
+  attr_accessor :player1, :player2, :board, :replay, :match_finished, :turns
 
   def initialize
     self.player1 = nil
     self.player2 = nil
-    self.grid = Board.new.grid
+    self.board = Board.new
     self.replay = true
     self.match_finished = false
     self.turns = 1
@@ -40,35 +40,63 @@ class Game
   end
 
   def start_game
-    while replay
+    while game_replay?
       play_once
       prompt_replay
       game_end
     end
   end
 
+  def game_replay?
+    return replay
+  end
+
   def play_once
-    until game_over? || grid_filled?(grid)
-      display_grid(grid)
-      turns.odd? ? turn_msg(player1) : turn_msg(player2)
+    until match_finished || grid_filled?(board.grid)
+      display_grid(board.grid)
+      turns.odd? ? turn_msg(player1.name) : turn_msg(player2.name)
       turns.odd? ? place_token(player1) : place_token(player2)
+      game_over?
+      self.turns += 1
     end
   end
 
   # condition that checks whether a token exists below the position indicated by grid_pos in get_grid_position
   def place_token(player)
-    pos_choice = get_grid_position
-    grid.update_grid(player, pos_choice)
+    pos_choice = get_grid_position(board.grid)
+    board.update_grid(player, pos_choice)
     player.update_token_locations(pos_choice)
+  end
+
+  def game_over?
+    p1_won = check_p_win(player1)
+    p2_won = check_p_win(player2)
+    if p1_won
+      winner_msg(player1.name)
+      match_finished = true
+    elsif p2_won
+      winner_msg(player2.name)
+      match_finished = true
+    elsif grid_filled?(board.grid)
+      game_draw_msg
+      match_finished = true
+    end
   end
 
   # TODO: implement game-resetting and game-winning condition checking
   def prompt_replay
-    return true
+    replay_choice = get_decision
+    replay = false if replay_choice == 'n'
   end
 
   def game_end
-    return false
+    replay ? game_reset : goodbye_msg
   end
 
+  def game_reset
+    self.turns = 1
+    self.replay = true
+    self.match_finished = false
+    board.reset_grid
+  end
 end
